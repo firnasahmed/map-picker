102d9a9bff9666cb3ec763df32d3014f5fc8dbbb
Firnas Ahamed <firnasahamedinfo@gmail.com>
2023-09-22 06:29:04 +0530

    Updated function call for location details

--- map-picker/src/LocationPicker.js
+++ map-picker/src/LocationPicker.js
@@ -4,13 +4,13 @@ class LocationPicker extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      selectedLocation: null,
+      selectedLocation: null, // To store place details
     };
   }
 
   componentDidMount() {
     // Initialize the Google Maps map
-    // AIzaSyARvJewxRJX9BLfJo3CsZXFBOz8ZI7AKWM
+    // Google map api AIzaSyARvJewxRJX9BLfJo3CsZXFBOz8ZI7AKWM
     const google = window.google; // Accessing the Google Maps API from the window object
     const mapOptions = {
       center: { lat: 8.00, lng: 81.00 }, // We are setting an initial center point in Sri Lanka
@@ -19,6 +19,7 @@ class LocationPicker extends Component {
     const map = new google.maps.Map(this.mapContainer, mapOptions);
 
     // Adding a click event listener to the map
+    map.setOptions({ draggableCursor: 'crosshair' }); // Set cursor to crosshair
     map.addListener('click', (event) => {
       // Getting the clicked location's coordinates
       const selectedLocation = {
@@ -26,24 +27,62 @@ class LocationPicker extends Component {
         lng: event.latLng.lng(),
       };
 
+      console.log(selectedLocation);
+
       // Update the state with the selected location
       this.setState({ selectedLocation });
+
+      // Update the iframe's src attribute with the new coordinates
+    const iframe = document.getElementById('embeddedMap');
+    iframe.src = `https://www.google.com/maps/embed/v1/place?q=${selectedLocation.lat},${selectedLocation.lng}&key=AIzaSyARvJewxRJX9BLfJo3CsZXFBOz8ZI7AKWM`;
     });
   }
 
+   // Function to update the embedded Google Map with new coordinates
+   updateEmbeddedMap = (latitude, longitude) => {
+    // Get a reference to the embedded map iframe
+    const iframe = document.getElementById('embeddedMap');
+
+    // Generate the URL for the embedded map using the coordinates
+    const embeddedMapUrl = `https://www.google.com/maps/@${latitude},${longitude},15z`;
+
+    // Set the iframe's src attribute to the new URL
+    iframe.src = embeddedMapUrl;
+  };
   render() {
     return (
-      <div>             
+        <div>
         <div
-            ref={(div) => (this.mapContainer = div)}
-            style={{ width: '100%', height: '800px' }}
-            />
-            {/* Display the selected address here */}
-            {this.state.selectedLocation && (
-            <p>
-                Selected Location: Lat: {this.state.selectedLocation.lat}, Lng: {this.state.selectedLocation.lng}
-            </p>
-            )}
+          ref={(div) => (this.mapContainer = div)}
+          style={{ width: '100%', height: '400px' }}
+        />
+        <div>
+          {/* Display place details if available */}
+          {this.state.selectedLocationDetails ? (
+            <div>
+              <h2>Place Details</h2>
+              <p>Name: {this.state.selectedLocationDetails.name}</p>
+              <p>Address: {this.state.selectedLocationDetails.formatted_address}</p>
+              {/* Add more details as needed */}
+            </div>
+          ) : (
+            // Display latitude and longitude if no place details available
+            <div>
+              <h2>Coordinates</h2>
+              <p>Latitude: {this.state.selectedLocation?.lat}</p>
+              <p>Longitude: {this.state.selectedLocation?.lng}</p>
+            </div>
+          )}
+        </div>
+        {/* Embedded Google Map */}
+        <iframe
+          id="embeddedMap"
+          width="600"
+          height="450"
+          frameborder="0"
+          style={{ border: 0 }}
+          allowfullscreen
+        ></iframe>
       </div>
     );
   }
